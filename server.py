5f46c731e9bd5615d258ea357fccb6178142e24d
Miron Barykin <barykinmiron@gmail.com>
2023-12-01 14:08:00 +0100

    redox reaction solving moved to the server

--- server.py
+++ server.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import csv
 import random
+
+class Element:
+    def __init__(self, name: str, before: int = 1, after_up_number: int = 1, after_up_symbol: str = '', after_down: int = 1) -> None:
+        self.name = name
+
+        self.before = before
+        self.after_up_number = after_up_number
+        self.after_up_symbol = after_up_symbol
+        self.after_down = after_down
+
+    def display(self) -> str:
+        a = f'{self.before if self.before != 1 else ""}{self.name}'
+        b = f'<span class="up">{self.after_up_number if self.after_up_number != 1 else ""}{self.after_up_symbol}</span>'
+        c = f'<span class="down">{self.after_down if self.after_down != 1 else ""}</span>'
+
+        return a + b + c
+
 
 app = Flask(__name__)
 
@@ -21,6 +38,36 @@ def generate():
     nichtmetall = random.choice([element for element in elements if element["M/N"] == "N"])
 
     return jsonify({"metall": metall, "nichtmetall": nichtmetall})
+
+
+@app.route('/result', methods=['GET', 'POST'])
+def result():
+    data = request.json
+
+    metall = Element(data['metall']['kurz'])
+    nichtmetall = Element(data['nichtmetall']['kurz'])
+
+    metall_left = Element(metall.name, after_down=int(data['metall']['molekule']))
+    metall_right = Element(metall.name, after_up_number=int(data['metall']['edelgaskonfiguration']), after_up_symbol='+')
+    electrons_right = Element('<i>e</i>', before = int(data['metall']['edelgaskonfiguration']), after_up_symbol='&#8722')
+
+    nichtmetall_left = Element(nichtmetall.name, after_down=int(data['nichtmetall']['molekule']))
+    nichtmetall_right = Element(nichtmetall.name, before=int(data['nichtmetall']['molekule']), after_up_number=int(data['nichtmetall']['edelgaskonfiguration']), after_up_symbol='&#8722')
+    electrons_left = Element('<i>e</i>', before = int(data['nichtmetall']['edelgaskonfiguration']) * int(data['nichtmetall']['molekule']), after_up_symbol='&#8722')
+    
+    oxidation = f'{metall_left.display()} &#10230 {metall_right.display()} + {electrons_right.display()}' 
+    reduktion = f'{nichtmetall_left.display()} + {electrons_left.display()} &#10230 {nichtmetall_right.display()}'
+
+    if electrons_left.before != electrons_right.before:
+        metall_left.before = metall_left.before * electrons_left.before
+        metall_right.before = metall_right.before * electrons_left.before
+
+        nichtmetall_left.before = nichtmetall_left.before * electrons_right.before
+        nichtmetall_right.before = nichtmetall_right.before * electrons_right.before
+
+    redox = f'{metall_left.display()} + {nichtmetall_left.display()} &#10230 {metall_right.display()} + {nichtmetall_right.display()}'
+    
+    return jsonify({'content': f'{oxidation} <br> {reduktion} <br> {redox}'})
 
 
 if __name__ == '__main__':
--- static/scripts.js
+++ static/scripts.js
@@ -55,71 +55,20 @@ function update(data) {
 
 function redox() {
     document.getElementById('redox-button').disabled = true
-
-    plus = '&#43'
-    minus = '&#8722'
-    arrow = '&#10230;'    
-    electron = `<i>e</i><span class="up">&#8722</span>`
-
-    oxidation = {
-        'left': '',
-        'right': '',
-        'electrons': '',
-        'charge': '',
-    }
-    reduktion = {
-        'left': '',
-        'right': '',
-        'electrons': '',
-        'charge': '',
-    }
-    
     const result_field = document.getElementById('redox');
-
-    if (previous_generations) {
-        metall = previous_generations[previous_generations.length - 1].metall
-        nichtmetall = previous_generations[previous_generations.length - 1].nichtmetall
-    } else {
-        result_field.innerHTML = 'Error.';
-        return
-    }
-
-    if (nichtmetall.kurz == 'S') {
-        reduktion['left'] = nichtmetall.kurz
-        molekule = ''
-        multiplier = 1
-    } else {
-        reduktion['left'] = nichtmetall.kurz + `<span class="down">2</span>`
-        molekule = 2
-        multiplier = 2
-    }
     
-    reduktion['electrons'] = nichtmetall.edelgaskonfiguration * multiplier + electron
-    reduktion['charge'] = nichtmetall.edelgaskonfiguration * multiplier
-
-    if (nichtmetall.edelgaskonfiguration == 1) {
-        nichtmetall.edelgaskonfiguration = ''
-    }
-
-    reduktion['right'] = molekule + nichtmetall.kurz + `<span class="up">${nichtmetall.edelgaskonfiguration}${minus}</span>`
-
-    oxidation['charge'] = metall.edelgaskonfiguration
-
-    if (metall.edelgaskonfiguration == 1) {
-        metall.edelgaskonfiguration = ''
-    }
-
-    oxidation['left'] = metall.kurz
-    oxidation['right'] = metall.kurz + `<span class="up">${metall.edelgaskonfiguration} ${plus}</span>`
-    oxidation['electrons'] = ` ${metall.edelgaskonfiguration + electron} `
-    
-    oxidation['result'] = `${oxidation['left']} ${arrow} ${oxidation['right']} + ${oxidation['electrons']}`
-    reduktion['result'] = `${reduktion['left']} + ${reduktion['electrons']} ${arrow} ${reduktion['right']}`
-
-    console.log(oxidation, metall)
-    console.log(reduktion, nichtmetall)
-
-    result_field.innerHTML =  oxidation['result'] + `(${oxidation['charge']} Elektronen)` + `<br>` + reduktion['result'] + `(${reduktion['charge']} Elektronen)`;
+    fetch('/result', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(previous_generations[previous_generations.length - 1]),
+    })
+    .then(response => response.json())
+    .then(data => {
+        result_field.innerHTML = data.content
+    })
+    .catch(error => console.error('Error:', error));
 }
 
 function salz() {
